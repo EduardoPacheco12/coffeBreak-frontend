58aa8d1bd44a44876b5faea1f237ff9e3a49e4fe
EduardoPacheco12 <eduardooliveirapacheco131@gmail.com>
2022-09-29 00:53:49 -0300

    feat: inital commit

---
+++ src/App.js
@@ -0,0 +1,7 @@
+import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
+import "./assets/reset.css";
+import "./assets/globalStyles.js";
+
+export default function App() {}
---
+++ src/assets/globalStyle.js
@@ -0,0 +1,12 @@
+import { createGlobalStyle } from "styled-components";
+
+const GlobalStyle = createGlobalStyle`
+  body {
+    margin: 0;
+    padding: 0;
+    background-color: #333333;
+    box-sizing: border-box;
+  }
+`;
+
+export default GlobalStyle;
